5fa2154e498f27629cd70f3178976e41bbb312f7
sreayan <sreayan69@gmail.com>
2024-09-12 13:34:43 +0530

    added alternate solution

--- Arrays_Strings/238.py
+++ Arrays_Strings/238.py
@@ -14,6 +14,24 @@ class Solution:
             right_mult*=nums[j]
 
         return [l*r for l,r in zip(l_arr,r_arr)]        
+###########################################
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1]*(len(nums))
 
+        prefix= 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix=nums[i]*prefix
+        postfix=1
+        for j in range(len(nums)-1,-1,-1):
+            res[j]*=postfix
+            postfix=postfix*nums[j]
+
+        return res        
+
+
+
+        
 
         
